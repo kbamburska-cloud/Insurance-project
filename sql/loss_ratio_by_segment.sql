677be47a35667bb336e0c646e73685edb16b712e
Kat Bamburska <k.bamburska@gmail.com>
2026-03-23 05:42:07 +0000

    Create loss_ratio_by_segment.sql

---
+++ sql/loss_ratio_by_segment.sql
@@ -0,0 +1,8 @@
+SELECT p.segment,
+       SUM(c.claim_amount) AS total_claims,
+       SUM(p.premium) AS total_premium,
+       SUM(c.claim_amount)*1.0 / SUM(p.premium) AS loss_ratio
+FROM policies p
+LEFT JOIN claims c 
+ON p.policy_id = c.policy_id
+GROUP BY p.segment;
