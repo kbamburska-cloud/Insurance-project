f6319dc8231ab65f7269f713a58da3b045e20c5a
Kat Bamburska <k.bamburska@gmail.com>
2026-03-24 05:21:21 +0000

    Create loss_ratio_by_band.sql
    Add loss ratio by premium band analysis

---
+++ sql/loss_ratio_by_band.sql
@@ -0,0 +1,14 @@
+SELECT 
+	   case
+	   	when premium >500 then 'HIGH'
+	   	when premium >= 300 then 'MIDIUM'
+	   	else 'LOW'
+	   end as premium_band,	   
+       SUM(c.claim_amount) AS total_claims,
+       SUM(p.premium) AS total_premium,
+       SUM(c.claim_amount)*1.0 / SUM(p.premium) AS loss_ratio
+FROM policies p
+LEFT JOIN claims c 
+ON p.policy_id = c.policy_id
+GROUP BY premium_band
+order by loss_ratio desc;
